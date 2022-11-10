11bf51217f68223a0a93361d3c5697b2847a20f4
zhikai-119 <qq1507438135@163.com>
2022-11-10 10:43:58 +0800

    laravel init

---
+++ app/Models/Chirp.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use App\Events\ChirpCreated;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Chirp extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<string>
+     */
+    protected $fillable = [
+        'message',
+    ];
+
+    /**
+     * The event map for the model.
+     *
+     * Allows for object-based events for native Eloquent events.
+     *
+     * @var array
+     */
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Controllers\ChirpController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+//Route::group(['prefix' => 'chirps', 'middleware' => ['auth', 'verified'], 'as' => 'chirps.', 'namespace' => 'App\\Http\\Controllers'], function () {
+//    Route::get('/', ['as' => 'list', 'uses' => 'ChirpController@index']);
+//    Route::post('/', ['as' => 'store', 'uses' => 'ChirpController@store']);
+//    Route::put('/{chirps}', ['as' => 'update', 'uses' => 'ChirpController@update']);
+//    Route::delete('/{chirps}', ['as' => 'delete', 'uses' => 'ChirpController@destroy']);
+//});
+Route::resource('chirps', ChirpController::class)
+    ->only(['index', 'store', 'update', 'destroy'])
+    ->middleware(['auth', 'verified']);
+
+require __DIR__.'/auth.php';
