8d79686d7aa1369207ea10133bccd443c904eb19
Donny <qq1507438135@163.com>
2023-04-21 16:48:11 +0800

    test rabbitmq

---
+++ app/Console/Commands/CreateUser.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\UserCreated;
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class CreateUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'create:user';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        UserCreated::dispatch(User::inRandomOrder()->first()->toArray());
+    }
+}
